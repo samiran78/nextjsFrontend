ef06a4cf8130f5837e6e340e0bfbb4e93cd39c6e
Samiran Royy <sroy21191@gmail.com>
2024-07-10 02:06:57 +0530

    add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:['images.unsplash.com','encrypted-tbn0.gstatic.com','static.toiimg.com','akm-img-a-in.tosshub.com']
+    }
+};
+
+export default nextConfig;
